afe2f9cc0574d5f4fba81b91074221937f57a025
ujjwalraajput <ujjwal.raajput@gmail.com>
2020-07-12 13:49:41 +0530

    a

--- ahrnm.py
+++ ahrnm.py
@@ -7,10 +7,11 @@ def k(w, i):
 d = { **{ k(w, 0):k(w, 1) for w in '''
 
 dcs/data https://raw.githubusercontent.com/OliverHellwig/sanskrit/master/dcs/data/rigveda/pada-and-analysis.dat
+spoken/spoken https://github.com/indic-dict/stardict-sanskrit/raw/d39a9aa2d33b24e19faf5a33522dbefb63677563/sa-head/en-entries/spokensanskrit/archives/spokensanskrit.gls
 
 '''.split('\n') if w }, **{ 'RV/RV' + n + '.html' : 'https://liberalarts.utexas.edu/lrc/rigveda/RV' + n + '.php' for n in [str(i).zfill(2) for i in range(1, 11)] } }
 
-for dir in 'RV dcs'.split(' '):
+for dir in 'RV dcs spoken'.split(' '):
 	if not os.path.exists(dir):
 		os.makedirs(dir)
 
@@ -18,5 +19,6 @@ for _o in d:
 	if not os.path.isfile(_o):
 		with ur.urlopen(d[_o]) as f:
 			with open(_o, 'w') as o:
+				print(_o)
 				o.write(f.read().decode('utf-8'))
 				print(_o)
--- sndanm.py
+++ sndanm.py
@@ -1,10 +1,63 @@
 import html
 import re
+import Levenshtein
+import unidecode
+
+def marjnm(s, m):
+	for n in m:
+		s = s.replace(n, '')
+	return s
 
 abcd = 'abcdefgh'
 
-d = [(int(l[0])*1000000 + int(l[1])*1000 + int(l[2])*10 + {abcd[i]:i for i in range(len(abcd))}[l[3]], l[4], l[5]) for l in [l.split('\t') for l in open('dcs/data').readlines()[1:]]]
+def n(a, b, c, d):
+	return int(a)*1000000 + int(b)*1000 + int(c)*10 + {abcd[i]:i for i in range(len(abcd))}[d]
 
-t = [i for l in [re.findall('">[0-9/.a-h]{9}<.*</span', html.unescape(open('RV/RV'+str(i).zfill(2)+'.html').read())) for i in range(1, 10)] for i in l]
+d = [(lambda l: (n(l[0], l[1], l[2], l[3]), l[4], l[5]))(l.split('\t')) for l in open('dcs/data').readlines()[1:]]
 
-print(set([i[2] for i in t]))
+suci = dict()
+
+#for 
+
+nre = '[0-9]{1,2}\\.[0-9]{3}\\.[0-9]{2}[a-z]'
+
+t = [((lambda x: n(x[:-8], x[-7:-4], x[-3:-1], x[-1]))(re.findall(nre, i)[0]), re.findall('[^>]*$', i[:-6])[0], re.findall('class=["\'][^"\']*["\']', i)[0][7:-2]) for l in [re.findall(nre + '.*</span', html.unescape(marjnm(open('RV/RV'+str(i).zfill(2)+'.html').read(), ['<sup>', '</sup>']))) for i in range(1, 11)] for i in l]
+
+def strnm(s):
+	return unidecode.unidecode(marjnm(s, ' '))
+
+def sndanm():
+	i = 0
+	j = 0
+	a = 0
+	s = 0
+
+	th = 3
+	u = 0
+
+	while i < len(d) and j < len(t):
+		if d[i][0] < t[j][0]:
+			i = i + 1
+		elif d[i][0] > t[j][0]:
+			j = j + 1
+		else:
+			s1 = strnm(d[i][1])
+			s2 = strnm(t[j][1])
+			l = Levenshtein.distance(s1, s2)
+			if l > len(s1) * 0.2:
+				u = u + 1
+				if u > th:
+					print(str(d[i][0]) + ', ' + s1 + ', ' + s2 + ', ' + str(l))
+					a = a + 1
+				else: s = s + 1
+			else:
+				s = s + 1
+				u = 0
+			i = i + 1
+			j = j + 1
+	print(a)
+	print(s)
+
+sndanm()
+
+print(set(u[2] for u in t))
