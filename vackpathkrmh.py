1e804b866b214b29a8c0527b0e020296736ba782
ujjwalraajput <ujjwal.raajput@gmail.com>
2021-01-24 15:17:39 +0530

    rust

--- vackpathkrmh.py
+++ vackpathkrmh.py
@@ -2,16 +2,31 @@ from indic_transliteration import sanscript
 import vedaweb
 rv = vedaweb.rv()
 
+mulpath=[]
 path=[]
 
 for m in rv.ekmekm():
 	for s in m:
-		def sodh(p):
-			q = p
-			for c in '॒॑' + ''.join([str(i) for i in range(9)]):
-				q = q.replace(c,'')
-			return q.replace('fgvedaH maRqalaM ','').strip()
-		path.append('\n'.join([sodh(sanscript.transliterate(r.source('gasuns'), sanscript.DEVANAGARI, sanscript.SLP1)) for r in s]))
+		for r in s:
+			def sodh(p):
+				q = p
+				for c in '॒॑' + ''.join([str(i) for i in range(9)]):
+					q = q.replace(c,'')
+				return q.replace('fgvedaH maRqalaM ','').strip()
+			vnh = r.source('vnh')
+			if vnh:
+				for c in vnh.split('\n'):
+					path.append(sanscript.transliterate(c, sanscript.IAST, sanscript.SLP1))
+					mulpath.append(c)
+			#path.append('\n'.join([sodh(sanscript.transliterate(c.smhita(), sanscript.IAST, sanscript.SLP1)) for c in r]))
+
+print(''.join(sanscript.schemes.roman.Slp1Scheme()['consonants']))
+print(''.join(sanscript.schemes.roman.Slp1Scheme()['vowels']))
+
+with open('slp', 'w+') as f:
+	f.write('\n'.join(path))
+with open('iast', 'w+') as f:
+	f.write('\n'.join(mulpath))
 
 class n_gramah:
 	def __init__(self, mhttmm_n):
@@ -22,7 +37,7 @@ class n_gramah:
 	def pathgramah(self, n, s):
 		for l in range(len(s) - n + 1):
 			w = s[l:l+n]
-			if not [c for c in w if c in ' \n\t-']:
+			if not [c for c in w if c in ' \n\t-'] and (n!=2 or (w[0] in sanscript.schemes.roman.Slp1Scheme()['consonants'] and w[1] in sanscript.schemes.roman.Slp1Scheme()['vowels'])):
 				yield w
 	
 	def gramsngrhnm(self, s):
@@ -52,7 +67,7 @@ def krmh():
 			if nvtvm:
 				kincidgrhitm = True
 				ptitani.append(si)
-				print(path[si])
+				print(mulpath[si])
 				print(m)
 				print(len(g.gramh[1].keys()))
 		if not kincidgrhitm:
---
+++ vackpathkrmh.rs
@@ -0,0 +1,108 @@
+use std::fs;
+
+fn main() {
+	const VYNJNANI: &str = "kKgGNcCjJYwWqQRtTdDnpPbBmyrlvSzshx";
+	const SVRGNAH: [&str; 4] = ["aAeEoO", "iI", "uU", "fF"];
+	const AKSRSNKYA: usize = VYNJNANI.len() * SVRGNAH.len();
+	type Aksrani = [u8; AKSRSNKYA];
+	
+	let mut vynjnkrmh: Vec<char> = VYNJNANI.chars().collect();
+	vynjnkrmh.sort();
+	let mut svrkrmh: Vec<char> = SVRGNAH.join("").chars().collect();
+	svrkrmh.sort();
+
+	let gnh: Vec<usize> = svrkrmh.iter()
+		.map(|c| {
+			for i in 1..SVRGNAH.len() {
+				if SVRGNAH[i].chars().any(|d| d == *c) {
+					return i;
+				}
+			}
+			0
+		})
+		.collect();
+	
+	let iast: Vec<String> = fs::read_to_string("iast")
+		.expect("file?!")
+		.split('\n')
+		.map(|s: &str| s.to_string())
+		.collect();
+
+	#[derive(Debug)]
+	struct Crnm {
+		ptitm: bool,
+		aksrani: Aksrani,
+		primanm: usize,
+	}
+
+	let mut slp: Vec<Crnm> = fs::read_to_string("slp")
+		.expect("file?!")
+		.split('\n')
+		.map(|s| {
+			let mut aksrani = [0; AKSRSNKYA];
+			let sc: Vec<char> = s.chars().collect();
+			for i in 0..sc.len()-1 {
+				let c = sc[i];
+				if i < s.len() - 1 {
+					match vynjnkrmh.binary_search(&c) {
+						Ok(n) => match svrkrmh.binary_search(&sc[i+1]) {
+							Ok(m) => aksrani[n*SVRGNAH.len() + gnh[m]] += 1,
+							Err(..) => {},
+						},
+						Err(..) => {},
+					}
+				}
+			}
+			Crnm { ptitm: false, aksrani: aksrani , primanm: sc.len() }
+		})
+		.collect();
+
+	let mut m = 0;
+	let mut ptitani = 0;
+	let mut aksrani = [0; AKSRSNKYA];
+	while ptitani < slp.len() {
+		let mut nvtvm = 1_f64;
+		while nvtvm > 0_f64 {
+			let nC = (0..slp.len()).fold((0_f64, None), |(M, C), D| {
+				if slp[D].ptitm {
+					(M, C)
+				}
+				else {
+					let N = (0..AKSRSNKYA).fold(0_usize, |n, i| {
+						n + if slp[D].aksrani[i] > 0 && aksrani[i] == m
+							{ 1 } else { 0 }
+					}) as f64 / slp[D].primanm as f64;
+
+					if N > M {
+						(N, Some(D))
+					}
+					else {
+						(M, C)
+					}
+				}
+			});
+			nvtvm = nC.0;
+			let C = nC.1;
+			match C {
+				Some(D) => {
+					ptitani += 1;
+					slp[D].ptitm = true;
+					for i in 0..AKSRSNKYA {
+						aksrani[i] += slp[D].aksrani[i];
+					}
+					println!("{}", iast[D]);
+					println!("{} {} {}", ptitani, aksrani.iter().fold(0, |n, x| {
+						n + if *x > m { 1 } else { 0 }
+					}), m);
+				},
+				None => {},
+			}
+		}
+		for i in 0..AKSRSNKYA {
+			if aksrani[i] == 0 {
+				println!("{} {}", vynjnkrmh[i/SVRGNAH.len()], i%SVRGNAH.len());
+			}
+		}
+		m += 1;
+	}
+}
