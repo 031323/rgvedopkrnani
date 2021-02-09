4692c8553f8c0d479c64fbb8b134ffe44e0a10b8
ujjvlh <ujjwal.raajput@gmail.com>
2021-02-09 12:18:44 +0530

    प॒द॒मू॒ल॒पा॒ठः

---
+++ rust/mulpath/src/main.rs
@@ -0,0 +1,13 @@
+use vedaweb;
+
+fn main() {
+    let args: Vec<_> = std::env::args().collect();
+    if args.len() != 2 {
+        println!("c-salt_vedaweb_tei ítyasyá sthā́nam ápekṣyate.");
+        std::process::exit(0);
+    }
+
+    let mndlani = vedaweb::aropnm(&args[1]).unwrap().0;
+    
+    std::fs::write("../rvmulani", mndlani.iter().flatten().flatten().map(|r| r.crnani.iter().flatten().map(|p| String::from(&p.mulm).replace(" ", "").replace("-", "").replace("√","").replace("~", "")).collect::<Vec<String>>().join(" ")).collect::<Vec<String>>().join("\n")).expect("!?");
+}
