e3d69699a536520d50ea54c757428085afddae95
ujjwalraajput <ujjwal.raajput@gmail.com>
2021-04-05 07:33:40 +0530

    a

--- iast235.py
+++ iast235.py
@@ -18,7 +18,21 @@ def iast235(s):
     t=t.replace('-', '')
     t=visrgh(t)
     t=sanscript.transliterate(t, sanscript.IAST, sanscript.SLP1).replace('x', 'L')
-    t=re.sub(r'(?=[aiuf][35]? ?)C', 'cC', t)
+    t=re.sub(r'(?<=[aiuf][35] )C', 'cC', t)
+    t=re.sub(r'(?<=[aiuf] )C', 'cC', t)
+    t=re.sub(r'(?<=[aiuf][35])C', 'cC', t)
+    t=re.sub(r'(?<=[aiuf])C', 'cC', t)
     t=re.sub(r'H(?= ?[kK])', 'Z', t)
     t=re.sub(r'H(?= ?[pP])', 'V', t)
     return t
+
+def iast2dev(s):
+    t=iast235(s)
+    t=t.replace('Z', 'H')
+    t=t.replace('V', 'H')
+    t=t.replace(' ', '')
+    t=re.sub(r'(?<=[aAiIuUfFeEoO])(?![35])', '॒', t)
+    t=t.replace('3', '')
+    t=t.replace('॒H', 'H॒')
+    t=t.replace('॒M', 'M॒')
+    return sanscript.transliterate(t, sanscript.SLP1, sanscript.DEVANAGARI)
