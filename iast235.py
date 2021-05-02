40e7006e32d7476dd90fde1acd01bce6463cd766
ujjwalraajput <ujjwal.raajput@gmail.com>
2021-05-02 16:13:49 +0530

    mityasvritm

--- iast235.py
+++ iast235.py
@@ -12,7 +12,7 @@ def iast235(s):
     t = s
     for c in "'()*3:[]~":
         t=t.replace(c, '-')
-    for p in 'á:a3 à:a5 í:i3 ì:i5 ú:u3 ù:u5 ŕ̥:r̥3 é:e3 è:e5 ó:o3 ò:o5 ́:3 \\:5 ̀:5 ṁ:ṃ Å:-u'.split(' '):
+    for p in 'á:a3 à:a5 í:i3 ì:i5 ú:u3 ù:u5 ŕ̥:r̥3 é:e3 è:e5 ó:o3 ò:o5 ́:3 \\:6 ̀:5 ṁ:ṃ Å:-u'.split(' '):
         ps = p.split(':')
         t=t.replace(ps[0], ps[1])
     t=t.replace('-', '')
@@ -28,6 +28,8 @@ def iast235(s):
 
 def iast2dev(s):
     t=iast235(s)
+    t=t.replace('5', '')
+    t=t.replace('6', '5')
     t=t.replace('Z', 'H')
     t=t.replace('V', 'H')
     t=t.replace(' ', '')
