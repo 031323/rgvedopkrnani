649d878a8077fe53e875b1e2579aff9d7e436fb9
ujjvlh <ujjwal.raajput@gmail.com>
2021-10-12 12:20:50 +0530

    udahrnani

--- rksvrprvrttih.py
+++ rksvrprvrttih.py
@@ -1,6 +1,6 @@
 import re
 
-rch = [r.split('।') for r in re.findall(r'(?<=[0-9])[^\n॥]+?(?=॥)', open('tmp').read())]
+rch = [r.split('।') for r in re.findall(r'[0-9]+\.[0-9]+\.[0-9]+[^\n॥]+?(?=॥)', open('tmp').read())]
 
 def udattanth(s):
 	return not re.findall(r'॑[^॒\n]*$', s)
@@ -8,27 +8,38 @@ def udattanth(s):
 def anudattanth(s):
 	return not udattanth(s)
 
-Uu = len([r for r in rch if len(r) > 1 and udattanth(r[-2]) and udattanth(r[-1])])
-Ua = len([r for r in rch if len(r) > 1 and udattanth(r[-2]) and anudattanth(r[-1])])
-Au = len([r for r in rch if len(r) > 1 and anudattanth(r[-2]) and udattanth(r[-1])])
-Aa = len([r for r in rch if len(r) > 1 and anudattanth(r[-2]) and anudattanth(r[-1])])
-uU = len([a for r in rch for a in r[:-1] if udattanth(a) and udattanth(r[-1])])
-aU = len([a for r in rch for a in r[:-1] if udattanth(a) and anudattanth(r[-1])])
-uA = len([a for r in rch for a in r[:-1] if anudattanth(a) and udattanth(r[-1])])
-aA = len([a for r in rch for a in r[:-1] if anudattanth(a) and anudattanth(r[-1])])
+u = [r for r in rch if len(r) == 1 and udattanth(r[-1])]
+a = [r for r in rch if len(r) == 1 and anudattanth(r[-1])]
 
-print(f'ए॒का॒किन॑ उ॒दात्ता॑न्ता उत्तमा॒र्धाः\t{len([r for r in rch if len(r) == 1 and udattanth(r[-1])])}')
-print(f'ए॒का॒किनोऽनु॑दात्तान्ता उत्तमा॒र्धाः\t{len([r for r in rch if len(r) == 1 and anudattanth(r[-1])])}')
-print(f'उ॒दात्ते॑ पूर्व्या॒र्धे स॒त्यु१॒॑दात्ता॑ उत्तमा॒र्धाः\t{Uu}')
-print(f'उ॒दात्ते॑ पूर्व्या॒र्धे स॒त्यनु॑दात्ता उत्तमा॒र्धाः\t{Ua}')
-print(f'अनु॑दात्ते पूर्व्या॒र्धे स॒त्यु१॒॑दात्ता॑ उत्तमा॒र्धाः\t{Au}')
-print(f'अनु॑दात्ते पूर्व्या॒र्धे स॒त्यनु॑दात्ता उत्तमा॒र्धाः\t{Aa}')
-print(f'उ॒दात्ता॑न्त उत्तमा॒र्धे स॒त्यु१॒॑दात्ता॑न्ता उत्तमेतरा॒र्धाः\t{uU}')
-print(f'उ॒दात्ता॑न्त उत्तमा॒र्धे स॒त्यनु॑दात्तान्ता उत्तमेतरा॒र्धाः\t{aU}')
-print(f'अनु॑दात्त उत्तमा॒र्धे स॒त्यु१॒॑दात्ता॑न्ता उत्तमेतरा॒र्धाः\t{uA}')
-print(f'अनु॑दात्त उत्तमा॒र्धे स॒त्यनु॑दात्तान्ता उत्तमेतरा॒र्धाः\t{aA}')
+Uu = [r for r in rch if len(r) > 1 and udattanth(r[-2]) and udattanth(r[-1])]
+Ua = [r for r in rch if len(r) > 1 and udattanth(r[-2]) and anudattanth(r[-1])]
+Au = [r for r in rch if len(r) > 1 and anudattanth(r[-2]) and udattanth(r[-1])]
+Aa = [r for r in rch if len(r) > 1 and anudattanth(r[-2]) and anudattanth(r[-1])]
+uU = [a for r in rch for a in r[:-1] if udattanth(a) and udattanth(r[-1])]
+aU = [a for r in rch for a in r[:-1] if udattanth(a) and anudattanth(r[-1])]
+uA = [a for r in rch for a in r[:-1] if anudattanth(a) and udattanth(r[-1])]
+aA = [a for r in rch for a in r[:-1] if anudattanth(a) and anudattanth(r[-1])]
 
-print(f'\nउ॒दात्ते॑ पूर्व्या॒र्धे स॒त्यु॑त्तमा॒र्धाना॑म् उदात्त॒त्वम्\t{Uu/(Uu+Ua)}')
-print(f'अनु॑दात्ते पूर्व्या॒र्धे स॒त्यु॑त्तमा॒र्धाना॑म् उदात्त॒त्वम्\t{Au/(Au+Aa)}')
-print(f'उ॒दात्त॑ उत्तमा॒र्धे स॒त्यु॑त्तमेतरा॒र्धाना॑म् उदात्त॒त्वम्\t{uU/(uU+aU)}')
-print(f'अनु॑दात्त उत्तमा॒र्धे स॒त्यु॑त्तमेतरा॒र्धाना॑म् उदात्त॒त्वम्\t{uA/(uA+aA)}')
+def nidrsnm(a):
+	def snkya(s):
+		for i, d in enumerate('०१२३४५६७८९'):
+			s = s.replace(str(i), d)
+		return s
+	return '\n' + "। ".join([re.sub(r"[0-9\.]*", '', b) for b in a[0]]) + '॥ ' + snkya(re.findall(r'[0-9\.]*', a[0][0])[0].replace('.', '॰')) + '॥\n'
+
+
+print(f'ए॒का॒किन॑ उ॒दात्ता॑न्ता उत्तमा॒र्धाः\t{len(u)}' + nidrsnm(u))
+print(f'ए॒का॒किनोऽनु॑दात्तान्ता उत्तमा॒र्धाः\t{len(a)}' + nidrsnm(a))
+print(f'उ॒दात्ते॑ पूर्व्या॒र्धे स॒त्यु१॒॑दात्ता॑ उत्तमा॒र्धाः\t{len(Uu)}' + nidrsnm(Uu))
+print(f'उ॒दात्ते॑ पूर्व्या॒र्धे स॒त्यनु॑दात्ता उत्तमा॒र्धाः\t{len(Ua)}' + nidrsnm(Ua))
+print(f'अनु॑दात्ते पूर्व्या॒र्धे स॒त्यु१॒॑दात्ता॑ उत्तमा॒र्धाः\t{len(Au)}' + nidrsnm(Au))
+print(f'अनु॑दात्ते पूर्व्या॒र्धे स॒त्यनु॑दात्ता उत्तमा॒र्धाः\t{len(Aa)}' + nidrsnm(Aa))
+print(f'उ॒दात्ता॑न्त उत्तमा॒र्धे स॒त्यु१॒॑दात्ता॑न्ता उत्तमेतरा॒र्धाः\t{len(uU)}' + nidrsnm([r for r in rch if [a for a in uU if a in r]]))
+print(f'उ॒दात्ता॑न्त उत्तमा॒र्धे स॒त्यनु॑दात्तान्ता उत्तमेतरा॒र्धाः\t{len(aU)}' + nidrsnm([r for r in rch if [a for a in aU if a in r]]))
+print(f'अनु॑दात्त उत्तमा॒र्धे स॒त्यु१॒॑दात्ता॑न्ता उत्तमेतरा॒र्धाः\t{len(uA)}' + nidrsnm([r for r in rch if [a for a in uA if a in r]]))
+print(f'अनु॑दात्त उत्तमा॒र्धे स॒त्यनु॑दात्तान्ता उत्तमेतरा॒र्धाः\t{len(aA)}' + nidrsnm([r for r in rch if [a for a in aA if a in r]]))
+
+print(f'\nउ॒दात्ते॑ पूर्व्या॒र्धे स॒त्यु॑त्तमा॒र्धाना॑म् उदात्त॒त्वम्\t{len(Uu)/(len(Uu)+len(Ua))}')
+print(f'अनु॑दात्ते पूर्व्या॒र्धे स॒त्यु॑त्तमा॒र्धाना॑म् उदात्त॒त्वम्\t{len(Au)/(len(Au)+len(Aa))}')
+print(f'उ॒दात्त॑ उत्तमा॒र्धे स॒त्यु॑त्तमेतरा॒र्धाना॑म् उदात्त॒त्वम्\t{len(uU)/(len(uU)+len(aU))}')
+print(f'अनु॑दात्त उत्तमा॒र्धे स॒त्यु॑त्तमेतरा॒र्धाना॑म् उदात्त॒त्वम्\t{len(uA)/(len(uA)+len(aA))}')
