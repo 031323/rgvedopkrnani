d563c95e9257394a6f96668b7ac166a138012c82
ujjvlh <ujjwal.raajput@gmail.com>
2025-02-03 19:14:41 +0530

    a

--- rust/yogah/src/lib.rs
+++ rust/yogah/src/lib.rs
@@ -1,6 +1,30 @@
 pub fn pdmulani(mndlani: &Vec<Vec<Vec<vedaweb::Rk>>>) -> (Vec<String>, Vec<i32>) {
         let mut pdmulani: Vec<String> = Vec::new();
+        let mut pdani: Vec<String> = Vec::new();
         let mut pdrgyogh: Vec<i32> = Vec::new();
+        let mut pdsuktyogh: Vec<i32> = Vec::new();
+
+        for s in mndlani.iter().flatten() {
+            let v = {
+                let mut v: Vec<String> = s
+                    .iter().map(|r| r.crnani.iter().flatten().map(|pdm| String::from(&pdm.rupm) + "\t" + &pdm.mulm)
+                    .collect::<Vec<String>>()).flatten().collect();
+                v.sort();
+                v.dedup();
+                v
+            };
+            for p in v {
+                match pdani.binary_search(&p) {
+                    Ok(pos) => {
+                        pdsuktyogh[pos] += 1;
+                    }
+                    Err(pos) => {
+                        pdani.insert(pos, p);
+                        pdsuktyogh.insert(pos, 1);
+                    }
+                }
+            }
+        }
 
         for r in mndlani.iter().flatten().flatten() {
             if r.strata != "A" {continue;}
@@ -29,10 +53,15 @@ pub fn pdmulani(mndlani: &Vec<Vec<Vec<vedaweb::Rk>>>) -> (Vec<String>, Vec<i32>)
             }
         }
         let mut krmh: Vec<usize> = (0..pdmulani.len()).collect();
+        let mut pdkrmh: Vec<usize> = (0..pdani.len()).collect();
+        pdkrmh.sort_by_key(|&k| -pdsuktyogh[k]);
         krmh.sort_by_key(|&k| -pdrgyogh[k]);
         println!("{:?}", krmh.len());
         for k in krmh {
-          println!("{}\t{}", pdmulani[k], pdrgyogh[k]);
+          //println!("{}\t{}", pdmulani[k], pdrgyogh[k]);
+        }
+        for k in pdkrmh {
+          println!("{}\t{}", pdani[k], pdsuktyogh[k]);
         }
         (pdmulani, pdrgyogh)
     }
